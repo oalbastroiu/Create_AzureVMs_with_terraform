6062a7306309a9cd4cbd0807262eb1da7bd4f441
oalbastroiu <albastroiu.ovidiu@gmail.com>
2022-11-15 15:36:35 +0200

    add remote backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,4 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "blob-storage-rg"
-    storage_account_name = "assessmentterraformstate"
-    container_name       = "statefile"
-    access_key           = "8yoGMI9rGwPoGL4/U0IC36sXG8m0lPOzBpF82MqHXLTTMvkOHPw4eAQTVSMKpZGTHz3SRfObJaYQ+AStcYgMrA=="
-    key                  = "terraform.tfstate"
   }
 }
